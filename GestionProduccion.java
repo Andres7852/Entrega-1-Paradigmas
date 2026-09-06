b8c77bcbc1722ee3bc257f54b8be9e4b1c5f0b5a
Daniel Morales <dm6932010@gmail.com>
2026-09-06 01:18:52 -0500

    feat: agregar métodos para auditoría de cierre de turno y ajuste porcentual de cantidades

--- GestionProduccion.java
+++ GestionProduccion.java
@@ -1,12 +1,38 @@
+import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
 
 public class GestionProduccion {
 
-	public static final BiConsumer<manufactura, Integer> agregarUnidades = (registro, adicionales) -> {
+    //metodo para modificar cantidades producidas
+    public static final BiConsumer<manufactura, Integer> agregarUnidades = (registro, adicionales) -> {
 		registro.setCantidad(registro.getCantidad() + adicionales);
 	};
 
+    //auditoria para el cierre de turno
+	public static final Consumer<manufactura> auditoriaCierreTurno = registro -> {
+		System.out.println("Cierre de turno - Codigo: " + registro.getCodigoProducto()
+				+ ", Producto: " + registro.getNombre()
+				+ ", Cantidad final: " + registro.getCantidad());
+	};
+
+	public static void cerrarTurno(List<manufactura> registros) {
+		registros.forEach(auditoriaCierreTurno);
+	}
+
+    //aplicar ajuste porcentual sobre un registro 
+	public static UnaryOperator<manufactura> ajustarCantidadPorcentaje(double porcentaje) {
+		return registro -> {
+			double nuevaCantidad = registro.getCantidad() * (1 + porcentaje / 100);
+			registro.setCantidad((int) Math.round(nuevaCantidad));
+			return registro;
+		};
+	}
+
+
+    //generar un registro de prduccion de prueba 
 	public static manufactura generarRegistroPrueba() {
 		Supplier<manufactura> registroPrueba = () -> new manufactura(
 				11,
